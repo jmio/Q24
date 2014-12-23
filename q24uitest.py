86f68dd6d353e36d2ac7b5c4610f09dd5fa71088
jmio <naoki@miolab.com>
2014-12-24 07:33:39 +0900

    TextView Scroll Change
    Change for UITextView Scrolling

--- q24uitest.py
+++ q24uitest.py
@@ -18,6 +18,7 @@ user = "target"
 password = "password"
 #EXECCOMMAND = "ls"
 
+@ui.in_background
 def connect(execcmd):
 	tn = telnetlib.Telnet(HOST)
 	v = tn.read_until("login: ",1)
@@ -27,41 +28,35 @@ def connect(execcmd):
 		v = tn.read_until("Password: ",1)
 		tn.write(password + "\r\n")
 		time.sleep(0.3)
-	printtext("Connect")
 	v = tn.read_until(">",1)
-	printtext("Execute")
-	print execcmd
 	tn.write(execcmd.encode('utf-8'))
 	tn.write('\r\n')
 	r = tn.read_until("->",20)
 	tn.close()
 	a = [i.strip("\n") for i in r.split("\r")[1:]]
-	printtext( "-- Result from "+HOST+" --")
 	for i in a:
 		printtext(i)
-	printtext("-- END --")
+	cmd.text=''
+	cmd.begin_editing()
 
 def printtext(s):
-	l = len(textview.text)
 	textview.begin_editing()
+	l = len(textview.text)
+	if not(l==0):
+		textview.selected_range=(l-1,l-1)
 	textview.replace_range((l,l),s+'\n')
 	l = len(textview.text)
 	textview.selected_range=(l-1,l-1)
-	cmd.begin_editing()
 
 def button_pushed(sender):
+	#for i in xrange(30):
 	#printtext(cmd.text)
 	connect(cmd.text)
-	cmd.text=''
-	cmd.begin_editing()
+	#cmd.text=''
+	#cmd.begin_editing()
 
 v = ui.load_view('q24uitest')
 textview = v['textview1']
 cmd = v['commandtext']
-if ui.get_screen_size()[1] >= 768:
-	# iPad
-	v.present('popover')
-else:
-	# iPhone
-	v.present(orientations=['portrait'])
+v.present('popover')
 
